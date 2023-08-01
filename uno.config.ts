95af678e81865ce405b79f7fae54607526898067
kuzushiki <kuzu7shiki@gmail.com>
2023-08-01 21:39:54 +0900

    fix: use latest version

---
+++ uno.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from 'unocss'
+
+export default defineConfig({
+  shortcuts: {
+    // custom the default background
+    'bg-main': 'bg-[#F5F3FF] text-[#4D1D95] dark:bg-[#4D1D95] dark:text-[#F5F3FF]',
+  },
+  // ...
+})
