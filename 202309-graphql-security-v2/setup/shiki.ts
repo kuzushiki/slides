c4a39ff5340ca6cf18f2c6d0836927827c7c8131
kuzushiki <kuzu7shiki@gmail.com>
2023-09-02 11:01:16 +0900

    add: setting shiki theme

---
+++ 202309-graphql-security-v2/setup/shiki.ts
@@ -0,0 +1,12 @@
+/* ./setup/shiki.ts */
+import { defineShikiSetup } from '@slidev/types'
+
+export default defineShikiSetup(() => {
+  return {
+    theme: {
+      dark: 'min-dark',
+      light: 'light-plus',
+      //light: 'min-light',
+    },
+  }
+})
